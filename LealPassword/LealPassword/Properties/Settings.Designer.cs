d788d6619518b505b363198270aecf8890940685
OnlyEduardo <eduardoleal.contact@gmail.com>
2021-04-08 12:23:31 -0300

    build 1.3.0.5

--- LealPassword/LealPassword/Properties/Settings.Designer.cs
+++ LealPassword/LealPassword/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace LealPassword.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.3.0.5")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
